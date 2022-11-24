c9678b0f91fe29bfc0a59f69d9fe73cc7ca30f8d
JC Pamo <josec.pamo@gmail.com>
2022-11-23 23:12:20 -0600

    Add error handling

--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -10,19 +10,28 @@ export class PokemonsComponent {
   firstData: any;
   pokemons: any[] = [];
   url = 'https://pokeapi.co/api/v2/pokemon/';
+  error: any;
 
   constructor(private apiFetch: ApiFetchService) {}
 
   ngOnInit() {
-    this.apiFetch.getData(this.url).subscribe((res) => {
-      this.firstData = res;
+    this.apiFetch.getData(this.url).subscribe(
+      (res) => {
+        this.firstData = res;
 
-      this.firstData.results.forEach((el: any) => {
-        this.apiFetch.getData(el.url).subscribe((res) => {
-          this.pokemons.push(res);
-          console.log(this.pokemons);
+        this.firstData.results.forEach((el: any) => {
+          this.apiFetch.getData(el.url).subscribe((res) => {
+            this.pokemons.push(res);
+          });
         });
-      });
-    });
+      },
+      (error) => {
+        this.error = {
+          error: error.ok,
+          status: error.status,
+          statusText: error.error,
+        };
+      }
+    );
   }
 }
